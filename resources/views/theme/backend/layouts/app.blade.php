f873b359f1d7f1c58dbcd9a6cb8fd73c5553a82e
Mridul Biswas <mridul.webengineer@gmail.com>
2017-08-27 11:02:12 +0530

    Admin theme Title fix

--- resources/views/theme/backend/layouts/app.blade.php
+++ resources/views/theme/backend/layouts/app.blade.php
@@ -71,7 +71,7 @@
             <div class="navbar-header">
                 <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                 <a href="javascript:void(0);" class="bars"></a>
-                <a class="navbar-brand" href="../../index.html">ADMINBSB - MATERIAL DESIGN</a>
+                <a class="navbar-brand" href="../../index.html">LARAVEL MULTI SHOP</a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
