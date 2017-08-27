b59d6f4bda2c63192a33f7f41ea5c6908cf02b05
Mridul Biswas <mridul.webengineer@gmail.com>
2017-08-27 09:26:32 +0530

    Product Variant Image and Specification Feature start

---
+++ app/Http/Controllers/ProductVariantImageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProductVariantImage;
+use Illuminate\Http\Request;
+
+class ProductVariantImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductVariantImage  $productVariantImage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductVariantImage $productVariantImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductVariantImage  $productVariantImage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductVariantImage $productVariantImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductVariantImage  $productVariantImage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductVariantImage $productVariantImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductVariantImage  $productVariantImage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductVariantImage $productVariantImage)
+    {
+        //
+    }
+}
---
+++ app/ProductVariantImage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariantImage extends Model
+{
+    //
+}
