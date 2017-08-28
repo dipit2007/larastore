083861c28279f9da6c622da5920240091e37550a
Mridul Biswas <mridul.webengineer@gmail.com>
2017-08-28 22:10:30 +0530

    migration error fix

--- database/migrations/2017_08_27_035430_create_product_variant_specification_features_table.php
+++ database/migrations/2017_08_27_035430_create_product_variant_specification_features_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductSpecificationFeaturesTable extends Migration
+class CreateProductVariantSpecificationFeaturesTable extends Migration
 {
     /**
      * Run the migrations.
