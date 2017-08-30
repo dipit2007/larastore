6a8d497677fdcf17c31120c389b20e6acd315bd7
Mridul Biswas <mridul.webengineer@gmail.com>
2017-08-30 22:00:46 +0530

    Seeder table start

---
+++ database/seeds/ProductVariantSpecificationTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductVariantSpecificationTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
